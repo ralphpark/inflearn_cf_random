ceb6dc77fd998301ebe97f9b72e4c2a849ef1039
Geunsoo Park <geunsoopark@Geunsooui-MacBookPro.local>
2024-02-12 02:41:10 +0900

    flutter random

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,45 @@
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children : [
+            const TestWidget(label: 'Text 1'),
+            const TestWidget(label: 'Text 2'),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text('Build')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key, required this.label});
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    print('$label 빌드 실행');
+    return Container(
+      child: Text(label),
+    );
   }
 }
