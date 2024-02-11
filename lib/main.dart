4569305bb3daf1327386a65dc77b1a0b218abc1f
Geunsoo Park <geunsoopark@Geunsooui-MacBookPro.local>
2024-02-12 02:01:54 +0900

    flutter random

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:inflearn_cf_random/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home : HomeScreen(),
+    )
+  );
+}
